21e3e02269bf00fbb63e50c71939fc09de708ced
inoueC <51142725+inoueC@users.noreply.github.com>
2019-05-29 15:39:58 +0900

    Update MatesRepository.java

--- src/main/java/jp/co/sss/psychology/repository/MatesRepository.java
+++ src/main/java/jp/co/sss/psychology/repository/MatesRepository.java
@@ -1,5 +1,7 @@
 package jp.co.sss.psychology.repository;
+import org.springframework.data.jpa.repository.JpaRepository; 
+import jp.co.sss.psychology.entity.Mates; 
 
-public class MatesRepository {
+public interface MatesRepository extends JpaRepository<Mates, Integer> {
 
 }
